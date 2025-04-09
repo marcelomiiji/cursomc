39160a74e4b8497afc9e65f3de717b77d3fe22dd
marcelomiiji <marcelomiiji@gmail.com>
2025-04-09 11:30:05 -0400

    Implementacao dos endpoint do restante das operacoes

--- src/main/java/com/tellis/cursomc/controllers/MathController.java
+++ src/main/java/com/tellis/cursomc/controllers/MathController.java
@@ -40,6 +40,66 @@ public class MathController {
  
 
     // localhost:8080/math/subtraction/3/5
+    @RequestMapping("/subtraction/{numberOne}/{numberTwo}")
+    public Double subtraction(
+        @PathVariable ("numberOne") String numberOne,
+        @PathVariable ("numberTwo") String numberTwo
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException(
+            "Please set a numeric value!"
+        );
+        return convertToDouble (numberOne) - convertToDouble(numberTwo);
+    }
+        
     // localhost:8080/math/division/3/5
+
+    @RequestMapping("/division/{numberOne}/{numberTwo}")
+    public Double division(
+        @PathVariable ("numberOne") String numberOne,
+        @PathVariable ("numberTwo") String numberTwo
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException(
+            "Please set a numeric value!"
+        );
+        return convertToDouble (numberOne) / convertToDouble(numberTwo);
+    }
+
+
     // localhost:8080/math/multiplication/3/5
+
+    @RequestMapping("/multiplication/{numberOne}/{numberTwo}")
+    public Double multiplication(
+        @PathVariable ("numberOne") String numberOne,
+        @PathVariable ("numberTwo") String numberTwo
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException(
+            "Please set a numeric value!"
+        );
+        return convertToDouble (numberOne) * convertToDouble(numberTwo);
+    }
+
+    // localhost:8080/math/pwr/3/5
+
+    @RequestMapping("/pwr/{numberOne}/{numberTwo}")
+    public Double pwr(
+        @PathVariable ("numberOne") String numberOne,
+        @PathVariable ("numberTwo") String numberTwo
+    ) throws Exception {
+        if(!isNumeric(numberOne) || !isNumeric(numberTwo)) throw new UnsupportedMathOperationException(
+            "Please set a numeric value!"
+        );
+        return Math.pow(convertToDouble (numberOne), convertToDouble(numberTwo));
+    }
+
+    // localhost:8080/math/sqrt/9
+    @RequestMapping("/sqrt/{numberOne}")
+    public Double sqrt(
+        @PathVariable ("numberOne") String numberOne
+    ) throws Exception {
+        if(!isNumeric(numberOne)) throw new UnsupportedMathOperationException(
+            "Please set a numeric value!"
+        );
+        return Math.sqrt(convertToDouble (numberOne));
+    }
+
 }
