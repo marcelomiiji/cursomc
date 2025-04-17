3a4cde22adf70ec5c7fcb677e978083fa6112493
marcelomiiji <marcelomiiji@gmail.com>
2025-04-17 07:16:47 -0400

    criado método DELETE do Person

--- src/main/java/com/tellis/cursomc/PersonServices.java
+++ src/main/java/com/tellis/cursomc/PersonServices.java
@@ -49,8 +49,18 @@ public class PersonServices {
 
     }
     
-    
-    
+    public Person update (Person person) {
+       
+       logger.info("Updating one person!");
+       return person;
+
+   }
+   
+   public void delete (String id) {
+
+         logger.info("Deleting one person!");
+         
+   }
     private Person mockPerson(int i) {
     
         Person person = new Person();
--- src/main/java/com/tellis/cursomc/controllers/PersonController.java
+++ src/main/java/com/tellis/cursomc/controllers/PersonController.java
@@ -38,4 +38,17 @@ public class PersonController {
     public Person create(@RequestBody Person person) {
         return service.create(person);
     }
+    
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+
+    public Person delete(@RequestBody Person person) {
+        return service.update(person);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+
+    public void delete(@PathVariable(value = "id") String id) {
+        service.delete(id);
+
+    }    
 }
